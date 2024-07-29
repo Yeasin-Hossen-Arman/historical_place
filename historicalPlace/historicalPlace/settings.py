c08831d02f7eb8f65f867dfef0ab5955b417613c
Yeasin-Hossen-Arman <yeasinhossenarman@gmail.com>
2024-07-30 00:05:13 +0600

    add

--- historicalPlace/historicalPlace/settings.py
+++ historicalPlace/historicalPlace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7^*$(083@9sa7w%$p3p!$_7*5n%^d!k_q-%j*r6_perb)e+=fl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
